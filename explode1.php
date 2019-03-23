a01c09e7b53e803497ff079b84f816926454944e
nefiaprasasti <44128833+nefiaprasasti@users.noreply.github.com>
2019-03-23 21:02:19 +0700

    Update explode1.php

--- explode1.php
+++ explode1.php
@@ -1,5 +1,5 @@
 <?php
-
+//contoh 1
 $data_awal="data 1 data2 data3 data4 data5";
 $pisah=explode(" ",$data_awal);
 echo "Data Asli :<br> $data_awal<hr><b> dipisah dengan pola spasi</b>";
@@ -10,3 +10,14 @@ echo "Potongan 4 : ".$pisah[3]."<br>";
 echo "Potongan 5 : ".$pisah[4]."<br>";
 
 ?>
+
+//contoh 2
+
+$tanggal="2015-10-20";
+$pecah_tgl=explode("-",$tanggal);
+$thn=$pecah_tgl[0];
+$bln=$pecah_tgl[1];
+$tgl=$pecah_tgl[2];
+
+echo $tgl."-".$bln."-".$thn;
+
